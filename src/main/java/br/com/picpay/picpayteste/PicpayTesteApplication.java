66bd88a5df582ca8c2ec201cf20b5e7c4eda0cc9
Alan Candido borges barreto <105753591+alancandidoborges@users.noreply.github.com>
2024-03-21 13:29:52 -0300

    Commite referente a estrutura do desafio
    Subindo a estrutura do desafio

---
+++ src/main/java/br/com/picpay/picpayteste/PicpayTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.picpay.picpayteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/picpay/picpayteste/PicpayTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.picpay.picpayteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
